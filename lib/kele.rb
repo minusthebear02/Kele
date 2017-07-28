c293b55a4b3f8bf91e3f58393e4f50668c086186
Mark Froehlich <mfroeh2@gmail.com>
2017-07-27 23:50:38 -0400

    Mentor Availability: Complete

--- lib/kele.rb
+++ lib/kele.rb
@@ -22,4 +22,9 @@ class Kele
     JSON.parse(response.body)
   end
 
+  def get_mentor_availability(mentor_id)
+    response = Kele.get("#{@base_uri}/mentors/#{mentor_id}/student_availability", headers: { "authorization" => @auth_token })
+    JSON.parse(response.body)
+  end
+
 end
