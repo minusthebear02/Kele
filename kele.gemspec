7e37247bb28ea64544d555a00d3940b5d9d0dbd4
Mark Froehlich <mfroeh2@gmail.com>
2017-07-27 22:48:14 -0400

    Branch fix

---
+++ kele.gemspec
@@ -0,0 +1,15 @@
+Gem::Specification.new do |s|
+  s.name          = 'Kele'
+  s.version       = '0.0.1'
+  s.date          = '2017-07-26'
+  s.summary       = 'Kele API Client'
+  s.description   = 'A client for the Bloc API'
+  s.authors       = ['Mark Froehlich']
+  s.email         = 'mfroeh2@gmail.com'
+  s.files         = ['lib/kele.rb']
+  s.require_paths = ["lib"]
+  s.homepage      = 'http://rubygems.org/gems/kele'
+  s.license       = 'MIT'
+  s.add_runtime_dependency 'httparty', '~> 0.13'
+  s.add_runtime_dependency 'json', '~> 1.8'
+end
---
+++ lib/kele.rb
@@ -0,0 +1,24 @@
+require 'httparty'
+require 'json'
+
+class Kele
+  include HTTParty
+
+  def initialize(email, password)
+    @base_uri = 'https://www.bloc.io/api/v1'
+    options = {body: {email: email, password: password}}
+    response = Kele.post("#{@base_uri}/sessions", options)
+    if response && response["auth_token"]
+      @auth_token = response["auth_token"]
+      puts "#{email} has sucessfully logged in"
+    else
+      puts "Login invalid"
+    end
+  end
+
+  def get_me
+    response = Kele.get("#{@base_uri}/users/me", headers: { "authorization" => @auth_token })
+    JSON.parse(response.body)
+  end
+
+end
